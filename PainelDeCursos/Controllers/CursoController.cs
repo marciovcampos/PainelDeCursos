b41bc9f799a9b93228c30312a4acd6478187a571
Marcio Vinicius <marcio.campos@outlook.com>
2018-08-03 23:42:25 -0300

    Adicionar arquivos de projeto.

---
+++ PainelDeCursos/Controllers/CursoController.cs
@@ -0,0 +1,45 @@
+﻿using PainelDeCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PainelDeCursos.Controllers
+{
+    public class CursoController : ApiController
+    {
+        // GET: api/Curso
+        public IEnumerable<Cursos> Get()
+        {
+            Cursos curso = new Cursos();
+
+            return curso.listaCursos();
+        }
+
+        // GET: api/Curso/5
+        public Cursos Get(int id)
+        {
+            Cursos curso = new Cursos();
+
+            return curso.listaCursos().Where(x => x.id == id).FirstOrDefault();
+
+        }
+
+        // POST: api/Curso
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Curso/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Curso/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PainelDeCursos/Models/Cursos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PainelDeCursos.Models
+{
+    public class Cursos
+    {
+        public int id { get; set; }
+        public string status { get; set; }
+        public string empresa { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public int qtdAlunos { get; set; }
+
+        public List<Cursos> listaCursos()
+        {
+
+            Cursos curso1 = new Cursos();
+            curso1.id = 1;
+            curso1.status = "Ativo";
+            curso1.empresa = "Empresa 1";
+            curso1.nome = "Curso 1";
+            curso1.descricao = "Descrição 1";
+            curso1.qtdAlunos = 128;
+
+            Cursos curso2 = new Cursos();
+            curso2.id = 2;
+            curso2.status = "Ativo";
+            curso2.empresa = "Empresa 2";
+            curso2.nome = "Curso 2";
+            curso2.descricao = "Descrição 2";
+            curso2.qtdAlunos = 256;
+
+            List<Cursos> listaCursos = new List<Cursos>();
+
+            listaCursos.Add(curso1);
+            listaCursos.Add(curso2);
+
+            return listaCursos;
+
+        }
+
+    }
+}
